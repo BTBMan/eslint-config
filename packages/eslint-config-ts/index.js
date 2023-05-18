e9c19778c251ef108b9955749a112e22b9ff361e
BTBMan <1498118157@qq.com>
2023-05-18 14:21:25 +0800

    feat: eslint-plugin-ts
    Signed-off-by: BTBMan <1498118157@qq.com>

---
+++ packages/eslint-config-ts/index.js
@@ -0,0 +1,34 @@
+const { defineConfig } = require('eslint-define-config');
+
+const tsconfig = process.env.ESLINT_TSCONFIG || 'tsconfig.eslint.json';
+
+module.exports = defineConfig({
+  extends: [
+    '@btbman/eslint-config-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    {
+      parserOptions: {
+        tsconfigRootDir: process.cwd(),
+        project: [tsconfig],
+      },
+      parser: '@typescript-eslint/parser',
+      files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
+      rules: {},
+    },
+  ],
+  rules: {
+    'typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/semi': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+});
